0d7e38efd9c332a7ac8d0c887c25009434f7be81
73455cONRaDJ <dazzles.hovels.0v@icloud.com>
2025-05-22 17:55:17 +0100

    Remove unused configuration settings from the file

--- 2SOvRW/ho3/hON-/confluence_qMPz_1747214152.md.py
+++ 2SOvRW/ho3/hON-/confluence_qMPz_1747214152.md.py
@@ -784,7 +784,6 @@ def aizhan_chaxun(ip, ua):
         'User-Agent': ua.random,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        'Accept-Encoding': 'gzip, deflate, br',
         'Referer': 'https://dns.aizhan.com/'}
     aizhan_url = 'https://dns.aizhan.com/' + str(ip) + '/'
     try:
