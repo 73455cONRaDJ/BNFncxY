8a1680cf7042f2f9799dce66aa9d3d71b505ca3c
73455cONRaDJ <dazzles.hovels.0v@icloud.com>
2025-05-23 03:38:56 +0100

    Add a new API endpoint in the file

--- 2SOvRW/ho3/hON-/confluence_qMPz_1747214152.md.py
+++ 2SOvRW/ho3/hON-/confluence_qMPz_1747214152.md.py
@@ -758,7 +758,6 @@ notebook.add(frameThree, text='IP反查域名+权重查询')
 def ip138_chaxun(ip, ua):
     ip138_headers = {
         'Host': 'site.ip138.com',
-        'User-Agent': ua.random,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
         'Accept-Encoding': 'gzip, deflate, br',
